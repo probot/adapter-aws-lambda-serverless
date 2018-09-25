3beb8d66dbc2d649d5707c6e7dfbf4a47386e4ee
Tommy Byrd <tcbyrd@github.com>
2018-09-24 23:16:58 -0400

    add test for handler

---
+++ tests/index.test.js
@@ -0,0 +1,37 @@
+const { serverless } = require('../')
+
+describe('serverless-lambda', () => {
+  let spy, handler, context
+
+  beforeEach(() => {
+    context = { done: jest.fn() }
+    spy = jest.fn()
+    handler = serverless(async app => {
+      app.auth = () => Promise.resolve({})
+      app.on('issues', spy)
+    })
+  })
+
+  it('responds with the homepage', async () => {
+    const event = { httpMethod: 'GET', path: '/probot' }
+    await handler(event, context)
+    expect(context.done).toHaveBeenCalled()
+    expect(context.done.mock.calls[0][0]).toMatchSnapshot()
+  })
+
+  it('calls the event handler', async () => {
+    const event = {
+      body: {
+        installation: { id: 1 }
+      },
+      headers: {
+        'x-github-event': 'issues',
+        'x-github-delivery': 123
+      }
+    }
+
+    await handler(event, context)
+    expect(context.done).toHaveBeenCalled()
+    expect(spy).toHaveBeenCalled()
+  })
+})
