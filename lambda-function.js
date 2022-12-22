193136b872d613c6f9ada4b98af36f4ec86e9cc2
AJ Schmidt <aschmidt@nvidia.com>
2022-12-22 13:46:24 -0500

    rm `try`/`catch` block

--- lambda-function.js
+++ lambda-function.js
@@ -3,29 +3,23 @@ module.exports = lambdaFunction;
 const lowercaseKeys = require("lowercase-keys");
 
 async function lambdaFunction(probot, event, context) {
-  try {
-    // lowercase all headers to respect headers insensitivity (RFC 7230 $3.2 'Header Fields', see issue #62)
-    const headersLowerCase = lowercaseKeys(event.headers);
+  // lowercase all headers to respect headers insensitivity (RFC 7230 $3.2 'Header Fields', see issue #62)
+  const headersLowerCase = lowercaseKeys(event.headers);
 
-    // this will be simpler once we ship `verifyAndParse()`
-    // see https://github.com/octokit/webhooks.js/issues/379
-    await probot.webhooks.verifyAndReceive({
-      id: headersLowerCase["x-github-delivery"],
-      name: headersLowerCase["x-github-event"],
-      signature:
-        headersLowerCase["x-hub-signature-256"] ||
-        headersLowerCase["x-hub-signature"],
-      payload: event.body,
-    });
+  // this will be simpler once we ship `verifyAndParse()`
+  // see https://github.com/octokit/webhooks.js/issues/379
+  await probot.webhooks.verifyAndReceive({
+    id: headersLowerCase["x-github-delivery"],
+    name: headersLowerCase["x-github-event"],
+    signature:
+      headersLowerCase["x-hub-signature-256"] ||
+      headersLowerCase["x-hub-signature"],
+    payload: event.body,
+  });
 
-    return {
-      statusCode: 200,
-      body: '{"ok":true}',
-    };
-  } catch (error) {
-    return {
-      statusCode: error.status || 500,
-      body: "Error processing GitHub Event",
-    };
-  }
+  return {
+    statusCode: 200,
+    body: '{"ok":true}',
+  };
+
 }
